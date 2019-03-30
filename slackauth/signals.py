80b77e550e89a9b57feefe04918e2c2cd3a28f13
kuratowski <tonyshin@kaist.ac.kr>
2019-03-30 21:44:22 +0900

    Fix signal initialization

--- slackauth/signals.py
+++ slackauth/signals.py
@@ -15,26 +15,17 @@ def save_slack_token(sender, **kwargs):
         assert isinstance(sociallogin, SocialLogin)
 
         if sociallogin.is_existing:
-
             app = sociallogin.token.app
 
             if app.provider == 'slack':
                 account = sociallogin.account
                 scopes = sociallogin.state.get('scope', '')
 
-                # TODO: Handle race conditions...
-                try:
-                    slack_token = SlackToken.objects.get(app=app, account=account, scopes=scopes)
-                except SlackToken.DoesNotExist:
-                    slack_token = SlackToken()
-
-                slack_token.app = app
-                slack_token.account = account
-                slack_token.token = sociallogin.token.token
-                slack_token.token_secret = sociallogin.token.token_secret
-                slack_token.expires_at = sociallogin.token.expires_at
-                slack_token.scopes = scopes
-                slack_token.save()
+                SlackToken.objects.update_or_create(defaults={
+                    'token': sociallogin.token.token,
+                    'token_secret': sociallogin.token.token_secret,
+                    'expires_at': sociallogin.token.expires_at,
+                }, app=app, account=account, scopes=scopes)
 
 
 def save_slack_data(sender, **kwargs):
@@ -58,26 +49,14 @@ def save_slack_data(sender, **kwargs):
                 user_data = account.extra_data.get('user', {})
                 team_data = account.extra_data.get('team', {})
 
-                # TODO: Handle race conditions...
-                try:
-                    team = SlackTeam.objects.get(pk=team_data.get('id', ''))
-                except SlackTeam.DoesNotExist:
-                    team = SlackTeam()
+                team, _ = SlackTeam.objects.update_or_create(defaults={
+                    'name': team_data.get('name'),
+                    'domain': team_data.get('domain'),
+                    'extra_data': team_data,
+                }, pk=team_data.get('id'))
 
-                team.id = team_data.get('id')
-                team.name = team_data.get('name', '')
-                team.domain = team_data.get('domain', '')
-                team.extra_data = team_data
-                team.save()
-
-                # TODO: Handle race conditions...
-                try:
-                    slack_account = SlackAccount.objects.get(account=account)
-                except SlackAccount.DoesNotExist:
-                    slack_account = SlackAccount()
-
-                slack_account.account = account
-                slack_account.slack_user_id = user_data.get('id', '')
-                slack_account.team = team
-                slack_account.extra_data = user_data
-                slack_account.save()
+                SlackAccount.objects.update_or_create(defaults={
+                    'slack_user_id': user_data.get('id'),
+                    'team': team,
+                    'extra_data': user_data,
+                }, account=account)
