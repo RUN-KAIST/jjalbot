88fc3b777ff62edc4a6c8799f3d8232efbde7531
kuratowski <tonyshin@kaist.ac.kr>
2019-04-04 02:20:27 +0900

    Divide Slack token model

--- slackauth/admin.py
+++ slackauth/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SlackToken, SlackTeam, SlackAccount
+from .models import SlackTeam, SlackAccount, SlackUserToken, SlackBotToken
 
 
 class SlackTeamAdmin(admin.ModelAdmin):
@@ -14,18 +14,32 @@ class SlackAccountAdmin(admin.ModelAdmin):
     list_filter = ('team', 'account__date_joined')
 
 
-class SlackTokenAdmin(admin.ModelAdmin):
-    raw_id_fields = ('app', 'account',)
-    list_display = ('app', 'account', 'truncated_scopes', 'token_type', 'expires_at')
-    list_filter = ('app', 'account', 'expires_at', 'token_type')
+class SlackUserTokenAdmin(admin.ModelAdmin):
+    raw_id_fields = ('app', 'slack_account',)
+    list_display = ('app', 'slack_account', 'truncated_scope', 'truncated_token')
+    list_filter = ('app', 'slack_account__team')
 
-    def truncated_scopes(self, token):
-        max_chars = 40
-        ret = token.scopes
+    def truncated_scope(self, token):
+        max_chars = 30
+        ret = token.scope
         if len(ret) > max_chars:
             ret = ret[0:max_chars] + '...(truncated)'
         return ret
-    truncated_scopes.short_description = 'Scopes'
+    truncated_scope.short_description = 'SlackUserToken'
+
+    def truncated_token(self, token):
+        max_chars = 30
+        ret = token.token
+        if len(ret) > max_chars:
+            ret = ret[0:max_chars] + '...(truncated)'
+        return ret
+    truncated_token.short_description = 'SlackUserToken'
+
+
+class SlackBotTokenAdmin(admin.ModelAdmin):
+    raw_id_fields = ('app', 'team',)
+    list_display = ('app', 'team', 'slack_bot_id', 'truncated_token')
+    list_filter = ('app', 'team')
 
     def truncated_token(self, token):
         max_chars = 40
@@ -33,9 +47,10 @@ class SlackTokenAdmin(admin.ModelAdmin):
         if len(ret) > max_chars:
             ret = ret[0:max_chars] + '...(truncated)'
         return ret
-    truncated_token.short_description = 'SlackToken'
+    truncated_token.short_description = 'SlackBotToken'
 
 
 admin.site.register(SlackTeam, SlackTeamAdmin)
-admin.site.register(SlackToken, SlackTokenAdmin)
 admin.site.register(SlackAccount, SlackAccountAdmin)
+admin.site.register(SlackUserToken, SlackUserTokenAdmin)
+admin.site.register(SlackBotToken, SlackBotTokenAdmin)
