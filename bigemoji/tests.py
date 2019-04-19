933fff153eeba106890f8b690dea66c8642711f4
kuratowski <tonyshin@kaist.ac.kr>
2019-04-20 02:23:38 +0900

    Introduce flake8

--- bigemoji/tests.py
+++ bigemoji/tests.py
@@ -1,4 +1,1 @@
-from django.test import TestCase
-
-# Create your tests here.
 # TODO: Write some tests.
--- slackauth/views.py
+++ slackauth/views.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
