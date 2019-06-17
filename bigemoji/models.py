33d189219cb8db8a2744d442cf15ad298dd5daed
kuratowski <tonyshin@kaist.ac.kr>
2019-06-18 08:56:49 +0900

    Add inline admin for BigEmoji

--- bigemoji/models.py
+++ bigemoji/models.py
@@ -37,9 +37,9 @@ class BigEmoji(models.Model):
     owner = models.ForeignKey(SlackAccount, on_delete=models.CASCADE)
     storage = models.ForeignKey(BigEmojiStorage, on_delete=models.CASCADE)
     emoji_name = models.CharField(max_length=100)
-    image_file = models.ImageField(upload_to=team_directory, null=True)
+    image_file = models.ImageField(upload_to=team_directory, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='date created')
-    alias = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    alias = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = (('storage', 'emoji_name'),)
