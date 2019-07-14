d280068a72d42b2037b6373b38de3c8b41ca3e9d
kuratowski <tonyshin@kaist.ac.kr>
2019-07-14 15:48:42 +0900

    Remove unnecessary condition

--- chatbot/tasks.py
+++ chatbot/tasks.py
@@ -26,19 +26,18 @@ def handle_message(req):
             format_dict = {
                 'user_id': user_id,
             }
-            if user_id != bot_token.slack_bot_id:
-                chat_manuals = team.chatmanualstorage.chatmanual_set.all()
-                for chat_manual in chat_manuals:
-                    m = re.match(chat_manual.trigger_re, text)
-                    if m:
-                        format_dict.update(m.groupdict())
-                        response = chat_manual.response.format(**format_dict)
-                        slack_api_call(
-                            'chat.postMessage', bot_token.token,
-                            channel=channel_id,
-                            as_user=True,
-                            text=response
-                        )
-                        break
+            chat_manuals = team.chatmanualstorage.chatmanual_set.all()
+            for chat_manual in chat_manuals:
+                m = re.match(chat_manual.trigger_re, text)
+                if m:
+                    format_dict.update(m.groupdict())
+                    response = chat_manual.response.format(**format_dict)
+                    slack_api_call(
+                        'chat.postMessage', bot_token.token,
+                        channel=channel_id,
+                        as_user=True,
+                        text=response
+                    )
+                    break
     except (SlackTeam.DoesNotExist, SlackBotToken.DoesNotExist):
         pass
