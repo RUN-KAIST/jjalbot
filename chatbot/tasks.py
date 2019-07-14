2fe01fa68238d0317199bcb130dded782ab59ed2
kuratowski <tonyshin@kaist.ac.kr>
2019-07-14 14:54:18 +0900

    Implement basic regex-based chat

--- chatbot/tasks.py
+++ chatbot/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 
+import re
+
 from slackauth.models import SlackBotToken, SlackTeam
 from slackauth.utils import slack_api_call
 
@@ -13,5 +15,21 @@ def handle_message(req):
     text = event.get('text')
     team = SlackTeam.objects.get(pk=team_id)
     bot_token = SlackBotToken.objects.get(team=team)
-    if user_id != bot_token.slack_bot_id and text == 'Hi!':
-        slack_api_call('chat.postMessage', bot_token.token, channel=channel_id, as_user=True, text='Hello!')
+
+    format_dict = {
+        'user_id': user_id,
+    }
+    if user_id != bot_token.slack_bot_id:
+        chat_manuals = team.chatmanualstorage.chatmanual_set.all()
+        for chat_manual in chat_manuals:
+            m = re.match(chat_manual.trigger_re, text)
+            if m:
+                format_dict.update(m.groupdict())
+                response = chat_manual.response.format(**format_dict)
+                slack_api_call(
+                    'chat.postMessage', bot_token.token,
+                    channel=channel_id,
+                    as_user=True,
+                    text=response
+                )
+                break
