d1ad1b89901d353a4150fd17143110f85698e08f
Joonhyung Shin <tonyshin@kaist.ac.kr>
2020-02-11 01:04:12 +0900

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,29 @@ docker build -t joonhyung/jjalbot:latest .
 printf "%s" "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USER}" --password-stdin
 docker push joonhyung/jjalbot:latest
 
+# Login to Portainer
+DOCKER_API_URL="${PORTAINER_API_URL}/endpoints/1/docker"
+PORTAINER_API_TOKEN=$(curl -f -X POST -H "Content-type: application/json" -d "{
+  \"Username\": \"${PORTAINER_USER}\",
+  \"Password\": \"${PORTAINER_PASSWORD}\"
+}" POST "${PORTAINER_API_URL}/auth" | jq .jwt)
+
 # Pull the new image
 X_REGISTRY_AUTH=$(printf '{
   "username": "%s",
   "password": "%s"
 }' "${DOCKER_USER}" "${DOCKER_PASSWORD}" | base64 | tr -d \\n)
-curl -X POST -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
+curl -X POST -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" \
      "${DOCKER_API_URL}/images/joonhyung%2Fjjalbot%3Alatest/tag?repo=joonhyung%2Fjjalbot&tag=legacy"
-curl -f -X POST -H "Authorization: Bearer ${DOCKER_API_TOKEN}" -H "X-Registry-Auth: ${X_REGISTRY_AUTH}" \
+curl -f -X POST -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" -H "X-Registry-Auth: ${X_REGISTRY_AUTH}" \
      "${DOCKER_API_URL}/images/create?fromImage=joonhyung%2Fjjalbot%3Alatest"
 
 # Recreate container through Docker Engine API
-curl -X POST -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
+curl -X POST -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" \
      "${DOCKER_API_URL}/containers/jjalbot/stop"
-curl -X DELETE -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
+curl -X DELETE -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" \
      "${DOCKER_API_URL}/containers/jjalbot"
-curl -f -X POST -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
+curl -f -X POST -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" \
      -H "Content-type: application/json" \
      -d \
       "{
@@ -55,9 +62,9 @@ curl -f -X POST -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
            ]
        }" \
      "${DOCKER_API_URL}/containers/create?name=jjalbot"
-curl -f -X POST -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
+curl -f -X POST -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" \
      "${DOCKER_API_URL}/containers/jjalbot/start"
      
 # Delete the old image
-curl -X DELETE -H "Authorization: Bearer ${DOCKER_API_TOKEN}" \
+curl -X DELETE -H "Authorization: Bearer ${PORTAINER_API_TOKEN}" \
      "${DOCKER_API_URL}/images/joonhyung%2Fjjalbot%3Alegacy"
